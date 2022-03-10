eff090f594b52bf479c9226df0961b325709da96
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2022-03-10 01:34:11 +0100

    Initlial commit

---
+++ app/src/main/java/pl/hanusik/pawel/pcstatus/NotificationFragment.java
@@ -0,0 +1,75 @@
+package pl.hanusik.pawel.pcstatus;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NotificationFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NotificationFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_TITLE = "title";
+    private static final String ARG_MESSAGE = "message";
+
+    // TODO: Rename and change types of parameters
+    private String title;
+    private String message;
+
+    public NotificationFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param title Parameter 1.
+     * @param message Parameter 2.
+     * @return A new instance of fragment NotificationFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NotificationFragment newInstance(String title, String message) {
+        NotificationFragment fragment = new NotificationFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TITLE, title);
+        args.putString(ARG_MESSAGE, message);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            this.title = getArguments().getString(ARG_TITLE);
+            this.message = getArguments().getString(ARG_MESSAGE);
+            this.title = "Test Title";
+            this.message = "Test message\nGASgajSG DNKSA";
+            Log.wtf("AAA", "onCreate");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_notification, container, false);
+        TextView title = (TextView)v.findViewById(R.id.title);
+        TextView message = (TextView)v.findViewById(R.id.message);
+        title.setText(this.title);
+        message.setText(this.message);
+        Log.wtf("AAA", "onCreateView");
+        return v;
+    }
+}
